79f366d8445cda2f7aaee856d84e95dfb7af7d12
unknown <bbdavidbb@gmail.com>
2020-04-13 16:38:35 -0700

    first

---
+++ puzzle.py
@@ -0,0 +1,57 @@
+import random
+
+# generate an nxn puzzle
+def gen_puzzle_n(n):
+    size = n * n
+    temp = list(range(size))
+    temp = sorted(temp, key=lambda x: random.random())
+    puzzle = [temp[i:i + n] for i in range(0, len(temp), n)]
+    return puzzle
+
+def move(puzzle, direction):
+    x, y = coord_of_tile(puzzle, 0)
+    x2, y2 = x + direction[0], y + direction[1]
+    if x2 not in range(len(puzzle)) or y2 not in range(len(puzzle[0])):
+        return None
+    puzzle[x][y], puzzle[x2][y2] = puzzle[x2][y2], puzzle[x][y]
+
+# find the 2d coordinates of the 0 tile
+def coord_of_tile(puzzle, tile_to_find):
+    for x, column in enumerate(puzzle):
+        for y, tile in enumerate(column):
+            if tile == tile_to_find:
+                return x, y
+def up(puzzle):
+    return move(puzzle, [1, 0])
+
+def down(puzzle):
+    return move(puzzle, [-1, 0])
+
+def left(puzzle):
+    return move(puzzle, [0, 1])
+
+def right(puzzle):
+    return move(puzzle, [0, -1])
+
+def printPuzzle(puzzle):
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle[i])):
+            print(puzzle[i][j], end=' ')
+        print()
+
+def main():
+    # some testing
+    test = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+
+    test2 = gen_puzzle_n(3)
+    printPuzzle(test)
+    right(test)
+    print("---------")
+    printPuzzle(test2)
+    print("---------")
+    up(test2)
+    printPuzzle(test2)
+
+
+if __name__ == "__main__":
+    main()
